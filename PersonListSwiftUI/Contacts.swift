ba6da953ecb04578140c657136a001c2054b93d7
JuliDeveloper <rja-97@yandex.ru>
2022-09-18 20:10:24 +0300

    Добавила два представления для отображения контактов; Добавила TabView в ContentView и настроила items

---
+++ PersonListSwiftUI/Contacts.swift
@@ -0,0 +1,20 @@
+//
+//  Contacts.swift
+//  PersonListSwiftUI
+//
+//  Created by Julia Romanenko on 18.09.2022.
+//
+
+import SwiftUI
+
+struct Contacts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonList_Previews: PreviewProvider {
+    static var previews: some View {
+        Contacts()
+    }
+}
--- PersonListSwiftUI/ContentView.swift
+++ PersonListSwiftUI/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Contacts()
+                .tabItem {
+                    Label("Contacts", systemImage: "person.3.fill")
+                }
+            Numbers()
+                .tabItem {
+                    Label("Numbers", systemImage: "phone.fill")
+                }
+        }
     }
 }
 
