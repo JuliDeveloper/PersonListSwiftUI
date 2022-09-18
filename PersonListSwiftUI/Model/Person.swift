9d4b822d0b979a4dde2babb551ad8a9836c38772
JuliDeveloper <rja-97@yandex.ru>
2022-09-18 19:52:11 +0300

    Добавила модель Person и написала функцию, возвращающую массив [Person]

---
+++ PersonListSwiftUI/Model/Person.swift
@@ -0,0 +1,47 @@
+//
+//  Person.swift
+//  PersonListSwiftUI
+//
+//  Created by Julia Romanenko on 18.09.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let lastname: String
+    let phone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(lastname)"
+    }
+}
+    
+extension Person {
+    static func getPerson() -> [Person] {
+        
+        let dataPerson = DataManager()
+        
+        var persons: [Person] = []
+        
+        let count = min(dataPerson.names.count,
+                        dataPerson.lastnames.count,
+                        dataPerson.phones.count,
+                        dataPerson.emails.count)
+        
+        for _ in 0..<count {
+            let name = dataPerson.names.randomElement() ?? ""
+            let lastname = dataPerson.lastnames.randomElement() ?? ""
+            let phone = dataPerson.phones.randomElement() ?? ""
+            let email = dataPerson.emails.randomElement() ?? ""
+            
+            persons.append(Person(name: name,
+                                  lastname: lastname,
+                                  phone: phone,
+                                  email: email))
+        }
+        
+        return persons
+    }
+}
